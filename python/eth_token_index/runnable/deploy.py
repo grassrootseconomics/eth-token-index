24cf85d7ead2ee315ff90b330a0f8611f18489b6
lash <dev@holbrook.no>
2023-02-11 08:26:44 +0000

    Upgrade deps, rehabilitate publish script

--- python/eth_token_index/runnable/deploy.py
+++ python/eth_token_index/runnable/deploy.py
@@ -16,6 +16,18 @@ import logging
 import chainlib.eth.cli
 from chainlib.chain import ChainSpec
 from chainlib.eth.tx import receipt
+from chainlib.eth.cli.arg import (
+        Arg,
+        ArgFlag,
+        process_args,
+        )
+from chainlib.eth.cli.config import (
+        Config,
+        process_config,
+        )
+from chainlib.eth.cli.log import process_log
+from chainlib.eth.settings import process_settings
+from chainlib.settings import ChainSettings
 
 # local imports
 from eth_token_index import TokenUniqueSymbolIndex
@@ -23,34 +35,57 @@ from eth_token_index import TokenUniqueSymbolIndex
 logging.basicConfig(level=logging.WARNING)
 logg = logging.getLogger()
 
-arg_flags = chainlib.eth.cli.argflag_std_write
-argparser = chainlib.eth.cli.ArgumentParser(arg_flags)
+
+arg_flags = ArgFlag()
+arg = Arg(arg_flags)
+flags = arg_flags.STD_WRITE 
+
+argparser = chainlib.eth.cli.ArgumentParser()
+argparser = process_args(argparser, arg, flags)
 args = argparser.parse_args()
 
-config = chainlib.eth.cli.Config.from_args(args, arg_flags, default_fee_limit=TokenUniqueSymbolIndex.gas())
+logg = process_log(args, logg)
 
-wallet = chainlib.eth.cli.Wallet()
-wallet.from_config(config)
+config = Config()
+config = process_config(config, arg, args, flags)
+#config = process_config_local(config, arg, args, flags)
+logg.debug('config loaded:\n{}'.format(config))
 
-rpc = chainlib.eth.cli.Rpc(wallet=wallet)
-conn = rpc.connect_by_config(config)
+settings = ChainSettings()
+settings = process_settings(settings, config)
+logg.debug('settings loaded:\n{}'.format(settings))
 
-chain_spec = ChainSpec.from_chain_str(config.get('CHAIN_SPEC'))
+
+
+#arg_flags = chainlib.eth.cli.argflag_std_write
+#argparser = chainlib.eth.cli.ArgumentParser(arg_flags)
+#args = argparser.parse_args()
+#
+#config = chainlib.eth.cli.Config.from_args(args, arg_flags, default_fee_limit=TokenUniqueSymbolIndex.gas())
+#
+#wallet = chainlib.eth.cli.Wallet()
+#wallet.from_config(config)
+#
+#rpc = chainlib.eth.cli.Rpc(wallet=wallet)
+#conn = rpc.connect_by_config(config)
+#
+#chain_spec = ChainSpec.from_chain_str(config.get('CHAIN_SPEC'))
 
 
 def main():
-    signer = rpc.get_signer()
-    signer_address = rpc.get_sender_address()
-
-    gas_oracle = rpc.get_gas_oracle()
-    nonce_oracle = rpc.get_nonce_oracle()
-
-    c = TokenUniqueSymbolIndex(chain_spec, signer=signer, gas_oracle=gas_oracle, nonce_oracle=nonce_oracle)
-
-    (tx_hash_hex, o) = c.constructor(signer_address)
-    if config.get('_RPC_SEND'):
+    conn = settings.get('CONN')
+    c = TokenUniqueSymbolIndex(
+            settings.get('CHAIN_SPEC'),
+            signer=settings.get('SIGNER'),
+            gas_oracle=settings.get('FEE_ORACLE'),
+            nonce_oracle=settings.get('NONCE_ORACLE'),
+            )
+    (tx_hash_hex, o) = c.constructor(
+        settings.get('SENDER_ADDRESS')
+        )
+    if settings.get('RPC_SEND'):
         conn.do(o)
-        if config.get('_WAIT'):
+        if config.true('_WAIT'):
             r = conn.wait(tx_hash_hex)
             if r['status'] == 0:
                 sys.stderr.write('EVM revert while deploying contract. Wish I had more to tell you')
